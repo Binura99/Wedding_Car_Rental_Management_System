4359f1aadac16f6c64f585555466d679b2f19ad9
Binura99 <binurayasith12@gmail.com>
2023-07-18 11:48:31 +0530

    Auth for Reservation

--- Backend/Routes/Reservations.js
+++ Backend/Routes/Reservations.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { Reservations } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.get("/", async (req, res) => {
     const listOfReservations = await Reservations.findAll();
     res.json(listOfReservations);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", validateToken, async (req, res) => {
     const reservation = req.body;
     await Reservations.create(reservation);
     res.json("Recorded Reservation");
